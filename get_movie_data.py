aa675ca04240f2e9efb3ca845f523ba71234e36d
vnoelifant <veronicanoel12490@gmail.com>
2022-09-27 22:54:19 -0500

    applying fixes and enhancements per 10-add-discover-methods

--- get_movie_data.py
+++ get_movie_data.py
@@ -15,53 +15,49 @@ BASE_URL = "https://api.themoviedb.org/3"
 COUNTRY_CODES = {country.name: country.alpha_2 for country in pycountry.countries}
 
 
-def get_movie_dict(endpoint, text_query):
+def get_movies(endpoint: str, text_query: str) -> dict[str, str]:
     """This function returns a dictionary of movie details based on a text query"""
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY, "query": text_query}
 
-    response = requests.get(
-        f"{BASE_URL}{endpoint}", params={"api_key": API_KEY, "query": text_query}
-    )
+    response = requests.get(url, params=params)
 
-    return response.json()
+    movies = {row["original_title"]: row["id"] for row in response.json()["results"]}
 
-
-def get_movie_id(movie_dict, movie_name):
-    """This function returns the movie ID for a specific movie title"""
-    for movie in movie_dict["results"]:
-        if movie["original_title"] == movie_name:
-            return movie["id"]
+    return movies
 
 
-def get_genre_dict(endpoint):
+def get_genres(endpoint: str) -> dict[str, str]:
     """This function returns a dictionary of movie genres"""
-    response = requests.get(f"{BASE_URL}{endpoint}", params={"api_key": API_KEY})
 
-    return response.json()
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY}
+
+    response = requests.get(url, params=params)
+
+    genres = {row["name"]: row["id"] for row in response.json()["genres"]}
+
+    return genres
 
 
-def get_genre_id(genre_dict, genre_name):
-    """This function returns the genre ID in list form for a specific genre name"""
-    for genre in genre_dict["genres"]:
-        if genre["name"] == genre_name:
-            return genre["id"]
-
-
-def get_most_popular(endpoint, language, page):
+def get_most_popular(endpoint, language, page=1):
     """This function returns a JSON object of the current most popular movies based on language"""
 
-    response = requests.get(
-        f"{BASE_URL}{endpoint}",
-        params={"api_key": API_KEY, "language": language, "page": page},
-    )
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY, "language": language, "page": page}
+    response = requests.get(url, params=params)
 
     return response.json()
 
 
-def get_top_rated(endpoint, region, page):
-    """This function returns a a JSON object of the first page of the top rated movies by region"""
+def get_top_rated(endpoint, region, page=1):
+    """This function returns a a JSON object of the top rated movies by region"""
+
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY, "region": region, "page": page}
     response = requests.get(
-        f"{BASE_URL}{endpoint}",
-        params={"api_key": API_KEY, "region": region, "page": page},
+        url,
+        params=params,
     )
 
     return response.json()
@@ -69,32 +65,39 @@ def get_top_rated(endpoint, region, page):
 
 def get_most_similar(endpoint, region):
     """This function returns a a JSON object of list of the most similar movies to movie ID based on region"""
-    response = requests.get(
-        f"{BASE_URL}{endpoint}", params={"api_key": API_KEY, "region": region}
-    )
+
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY, "region": region}
+    response = requests.get(url, params=params)
 
     return response.json()
 
 
 def get_recommended(endpoint, region):
     """This function returns a a JSON object of list of recommended movies to movie ID based on region"""
-    response = requests.get(
-        f"{BASE_URL}{endpoint}", params={"api_key": API_KEY, "region": region}
-    )
+
+    url = f"{BASE_URL}{endpoint}"
+    params = {"api_key": API_KEY, "region": region}
+
+    response = requests.get(url, params=params)
 
     return response.json()
 
 
 def get_recently_released(endpoint, region, date_min, date_max):
     """This function returns a JSON object of movies based on a region within a recent date range"""
+
+    url = f"{BASE_URL}{endpoint}"
+    params = params = {
+        "api_key": API_KEY,
+        "region": region,
+        "primary_release_date.gte": date_min,
+        "primary_release_date.lte": date_max,
+    }
+
     response = requests.get(
-        f"{BASE_URL}{endpoint}",
-        params={
-            "api_key": API_KEY,
-            "region": region,
-            "primary_release_date.gte": date_min,
-            "primary_release_date.lte": date_max,
-        },
+        url,
+        params=params,
     )
 
     return response.json()
@@ -102,82 +105,94 @@ def get_recently_released(endpoint, region, date_min, date_max):
 
 def get_year_genre(endpoint, region, year, genre_id):
     """This function returns a JSON object of movies based on region, primary release year, and genre"""
+
+    url = f"{BASE_URL}{endpoint}"
+    params = {
+        "api_key": API_KEY,
+        "region": region,
+        "primary_release_year": year,
+        "with_genres": genre_id,
+    }
+
     response = requests.get(
-        f"{BASE_URL}{endpoint}",
-        params={
-            "api_key": API_KEY,
-            "region": region,
-            "primary_release_year": year,
-            "with_genres": genre_id,
-        },
+        url,
+        params=params,
     )
 
+    return response.json()
 
-def get_vote_popular(endpoint, year, vote_count, page):
-    """This function returns a JSON object of movies greater than vote count, sorted by popularity"""
+
+def get_vote_sorted(endpoint, vote_count, year, sort_option, page=1):
+    """This function returns a JSON object of movies greater than vote count by year, sorted by selected sort option"""
+
+    url = f"{BASE_URL}{endpoint}"
+    params = {
+        "api_key": API_KEY,
+        "vote_count_gte": vote_count,
+        "year": year,
+        "sort_by": sort_option,
+        "page": page,
+    }
+
     response = requests.get(
-        f"{BASE_URL}{endpoint}",
-        params={
-            "api_key": API_KEY,
-            "primary_release_year": year,
-            "vote_count_gte": vote_count,
-            "page": page,
-        },
+        url,
+        params=params,
     )
+
+    return response.json()
 
 
 def main():
-    # Get most popular movies in English Language
-    most_popular_english = get_most_popular(
-        "/movie/popular", standardize_tag("eng_US"), 1
-    )
+
+    # Get first page of most popular movies in English Language
+    eng_lang = standardize_tag("eng_US")
+    most_popular_english = get_most_popular("/movie/popular", eng_lang)
     pprint(most_popular_english)
 
     # Get first page of top rated movies abased on German region
-    top_rated_german = get_top_rated("/movie/top_rated", COUNTRY_CODES["Germany"], 1)
+    germ_region = COUNTRY_CODES.get("Germany")
+    top_rated_german = get_top_rated("/movie/top_rated", germ_region)
     pprint(top_rated_german)
 
     # Get a dictionary of movie details based on text query
-    movie_dict = get_movie_dict("/search/movie", "Lost in Translation")
+    movies = get_movies("/search/movie", "Lost in Translation")
 
-    # Get movie idea based on selected movie title
-    movie_id = get_movie_id(movie_dict, "Lost in Translation")
+    # Get movie id based on selected movie title
+    movie_id = movies.get("Lost in Translation")
+
+    # Get US region
+    us_region = COUNTRY_CODES.get("United States")
 
     # Get most similar movies to movie with selected title based on US region
-    most_similar = get_most_similar(
-        f"/movie/{movie_id}/similar", COUNTRY_CODES["United States"]
-    )
+    most_similar = get_most_similar(f"/movie/{movie_id}/similar", us_region)
     pprint(most_similar)
 
     # Get recommended movies to movie ID based on US region
-    recommended = get_recommended(
-        f"/movie/{movie_id}/recommendations", COUNTRY_CODES["United States"]
-    )
+    recommended = get_recommended(f"/movie/{movie_id}/recommendations", us_region)
     pprint(recommended)
 
     # Get movies within a recent date range based on US region
     recently_released = get_recently_released(
-        "/discover/movie", COUNTRY_CODES["United States"], "2022-08-16", "2022-09-23"
+        "/discover/movie", us_region, "2022-08-16", "2022-09-23"
     )
 
     pprint(recently_released)
 
     # Get a dictionary of available genres
-    genre_dict = get_genre_dict("/genre/movie/list")
+    genres = get_genres("/genre/movie/list")
 
-    # Get a genre ID based on selected genre name
-    genre_id = get_genre_id(genre_dict, "Comedy")
-    # print(genre_id) # This prints an int 35
+    # Get comedy genre ID
+    com_genre_id = genres.get("Comedy")
 
-    # Get movies released by year based on US region and genre
-    movie_year_genre = get_year_genre(
-        "/discover/movie", COUNTRY_CODES["United States"], 1990, str([genre_id])
-    )
+    # Get movies released by year based on US region and comedy genre
+    movie_year_genre = get_year_genre("/discover/movie", us_region, 1990, com_genre_id)
 
     pprint(movie_year_genre)
 
-    # Get movies with vote count greater than 2000 by year, sorted by popularity
-    movie_vote_popular = get_vote_popular("/discover/movie", 2021, 2000, 1)
+    # Get first page of movies with vote count greater than 2000 by year, sorted by ascending popularity
+    movie_vote_popular = get_vote_sorted(
+        "/discover/movie", 2000, 2008, "popularity_asc"
+    )
 
     pprint(movie_vote_popular)
 
