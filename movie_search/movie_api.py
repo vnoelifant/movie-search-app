0f5797a5ffc705aad0da0d5050243556c5449d66
vnoelifant <veronicanoel12490@gmail.com>
2022-10-02 17:25:02 -0500

    adding a couple of url patterns and small refacting in api code

--- movie_search/movie_api.py
+++ movie_search/movie_api.py
@@ -6,13 +6,14 @@ from functools import partial
 import requests
 import pycountry
 from dotenv import load_dotenv
-from langcodes import standardize_tag
 
 load_dotenv()
 
 API_KEY = os.getenv("PROJECT_API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
 COUNTRY_CODES = {country.name: country.alpha_2 for country in pycountry.countries}
+LANG_ENG = 'en-US'
+REGION_US = COUNTRY_CODES.get("United States")
 
 
 def get_movies(endpoint: str, text_query: str) -> dict[str, int]:
@@ -40,7 +41,7 @@ def get_genres(endpoint: str) -> dict[str, int]:
     return genres
 
 
-def get_most_popular(endpoint, language, page=1):
+def get_most_popular(endpoint, language=LANG_ENG, page=1):
     """This function returns a JSON object of the current most popular movies based on language"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -50,7 +51,7 @@ def get_most_popular(endpoint, language, page=1):
     return response.json()
 
 
-def get_top_rated(endpoint, region, page=1):
+def get_top_rated(endpoint, region=REGION_US, page=1):
     """This function returns a a JSON object of the top rated movies by region"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -63,7 +64,7 @@ def get_top_rated(endpoint, region, page=1):
     return response.json()
 
 
-def get_most_similar(endpoint, region):
+def get_most_similar(endpoint, region=REGION_US):
     """This function returns a a JSON object of list of the most similar movies to movie ID based on region"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -73,7 +74,7 @@ def get_most_similar(endpoint, region):
     return response.json()
 
 
-def get_recommended(endpoint, region):
+def get_recommended(endpoint, region=REGION_US):
     """This function returns a a JSON object of list of recommended movies to movie ID based on region"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -84,7 +85,7 @@ def get_recommended(endpoint, region):
     return response.json()
 
 
-def get_recently_released(endpoint, region, date_min, date_max):
+def get_recently_released(endpoint, date_min, date_max, region=REGION_US,):
     """This function returns a JSON object of movies based on a region within a recent date range"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -103,7 +104,7 @@ def get_recently_released(endpoint, region, date_min, date_max):
     return response.json()
 
 
-def get_year_genre(endpoint, region, year, genre_id):
+def get_year_genre(endpoint, year, genre_id, region=REGION_US):
     """This function returns a JSON object of movies based on region, primary release year, and genre"""
 
     url = f"{BASE_URL}{endpoint}"
@@ -145,8 +146,7 @@ def get_vote_sorted(endpoint, vote_count, year, sort_option, page=1):
 def main():
 
     # Get first page of most popular movies in English Language
-    eng_lang = standardize_tag("eng_US")
-    most_popular_english = get_most_popular("/movie/popular", eng_lang)
+    most_popular_english = get_most_popular("/movie/popular")
     pprint(most_popular_english)
 
     # Get first page of top rated movies abased on German region
@@ -164,16 +164,16 @@ def main():
     us_region = COUNTRY_CODES.get("United States")
 
     # Get most similar movies to movie with selected title based on US region
-    most_similar = get_most_similar(f"/movie/{movie_id}/similar", us_region)
+    most_similar = get_most_similar(f"/movie/{movie_id}/similar")
     pprint(most_similar)
 
     # Get recommended movies to movie ID based on US region
-    recommended = get_recommended(f"/movie/{movie_id}/recommendations", us_region)
+    recommended = get_recommended(f"/movie/{movie_id}/recommendations")
     pprint(recommended)
 
     # Get movies within a recent date range based on US region
     recently_released = get_recently_released(
-        "/discover/movie", us_region, "2022-08-16", "2022-09-23"
+        "/discover/movie", "2022-08-16", "2022-09-23"
     )
 
     pprint(recently_released)
@@ -185,7 +185,7 @@ def main():
     com_genre_id = genres.get("Comedy")
 
     # Get movies released by year based on US region and comedy genre
-    movie_year_genre = get_year_genre("/discover/movie", us_region, 1990, com_genre_id)
+    movie_year_genre = get_year_genre("/discover/movie", 1990, com_genre_id)
 
     pprint(movie_year_genre)
 
--- movie_search/urls.py
+++ movie_search/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path("search/", views.search, name='search'),
+    path("movies/", views.movies, name='movies')
 ]
