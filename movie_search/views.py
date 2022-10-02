00ce4dc88e92bf280452758767e09ad28b68ec61
vnoelifant <veronicanoel12490@gmail.com>
2022-10-02 16:41:46 -0500

    adding supporting django folders

--- movie_search/views.py
+++ movie_search/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# This is a test
 def home(request):
-    return HttpResponse("This is a test for movie search app skeleton!")
+    return render(request, 'home.html')
