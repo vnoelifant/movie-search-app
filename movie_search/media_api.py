17cd4802f5e979906e14980fa709d876509cd0ff
vnoelifant <veronicanoel12490@gmail.com>
2022-10-19 21:08:38 -0500

    added string type notation for type parameter to get_media function

--- movie_search/media_api.py
+++ movie_search/media_api.py
@@ -16,7 +16,7 @@ LANG_ENG = "en-US"
 REGION_US = COUNTRY_CODES.get("United States")
 
 
-def get_media(endpoint: str, text_query: str, type, year: int = None) -> dict[str, int]:
+def get_media(endpoint: str, text_query: str, type: str, year: int = None) -> dict[str, int]:
     """This function returns a dictionary of media details based on a text query"""
     url = f"{BASE_URL}{endpoint}"
     params = {"api_key": API_KEY, "query": text_query}
