5b9fb974962d167a564408ffee2d9e34dec30687
vnoelifant <veronicanoel12490@gmail.com>
2022-11-02 01:09:26 -0500

    changed years tag to filter with range

--- movie_search/templatetags/years.py
+++ movie_search/templatetags/years.py
@@ -2,12 +2,15 @@ import datetime
 from django import template
 
 register = template.Library()
+    
+@register.filter(name='range')
+def _range(_start, args=None):
+    _stop, _step = None, None
+    if args:
+        if not isinstance(args, int):
+            _stop, _step = map(int, args.split(','))
+        else:
+            _stop = args
+    args = filter(None, (_start, _stop, _step))
 
-@register.simple_tag
-def get_years():
-
-    year_dropdown = []
-    for year in range((datetime.datetime.now().year + 2), 1940, -1):
-        year_dropdown.append(year)
-
-    return year_dropdown
+    return range(*args)
