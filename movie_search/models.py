332695385795dc2940efe9e0329392dbbefc8891
vnoelifant <veronicanoel12490@gmail.com>
2022-11-02 22:53:15 -0500

    changed String for representing thGenre model object

--- movie_search/models.py
+++ movie_search/models.py
@@ -4,9 +4,10 @@ from django.db import models
 from django.db import models
 
 class Genre(models.Model):
-    name = models.CharField(null=True)
-    id =   models.IntegerField(null=True)
+    name = models.CharField(max_length=200)
+    id =  models.AutoField(primary_key=True)
     
     def __str__(self):
-        return self.name
+        """String for representing the Model object."""
+        return f"{self.id}, {self.name}"
 
--- movie_search/templatetags/genres.py
+++ movie_search/templatetags/genres.py
@@ -7,34 +7,13 @@ register = template.Library()
 
 @register.simple_tag
 def get_genres():
-    # Get list of available genres
-    genre = Genre()
-    list_to_store = [
-        "Action",
-        "Adventure",
-        "Animation",
-        "Comedy",
-        "Crime",
-        "Documentary",
-        "Drama",
-        "Family",
-        "Fantasy",
-        "Horror",
-        "Music",
-        "Mystery",
-        "Romance",
-        "Science Fiction",
-        "TV Movie",
-        "Thriller",
-        "War",
-        "Western",
-    ]
-    genre.genres = json.dumps(list_to_store)
-    genre.save()
 
-    json_dec = json.decoder.JSONDecoder()
-    genre_list = json_dec.decode(genre.genres)
+    genres = Genre.objects.all()
 
-    print("Genre List: ", genre_list)
+    print("Genres: ", genres)
+    genre_names = [genre.name for genre in genres]
+    print("Genre Names", genre_names)
+    genre_ids = [genre.id for genre in genres]
+    print("Genre IDs", genre_ids)
 
-    return genre_list
+    return genre_names
