745c03e9e4822def104357d951c77d2045f98cd3
vnoelifant <veronicanoel12490@gmail.com>
2023-09-21 18:24:19 -0500

    moved banner style to styles.css file

--- movie_search/media_api.py
+++ movie_search/media_api.py
@@ -8,9 +8,7 @@ import requests
 from django.conf import settings
 
 BASE_URL = "https://api.themoviedb.org/3"
-COUNTRY_CODES = {country.name: country.alpha_2 for country in pycountry.countries}
 LANG_ENG = "en-US"
-REGION_US = COUNTRY_CODES.get("United States")
 
 def get_data_from_endpoint(endpoint, **kwargs):
     """This function returns a JSON object of tmdb media data"""
@@ -34,19 +32,3 @@ def get_person(endpoint, text_query):
     data = response_json["results"]
     return {row["name"]: row["id"] for row in data}
 
-def get_genres(endpoint: str) -> dict[str, int]:
-    """Returns a dictionary of movie genres"""
-    response_json = get_data_from_endpoint(endpoint)
-    return {row["name"]: row["id"] for row in response_json["genres"]}
-
-
-def main():
-    """These function calls are used for debugging/testing purposes"""
-    from utils import dump_movie_data_to_json
-    data = get_data_from_endpoint("/movie/153/recommendations")
-    print("Dumping data to json file")
-    dump_movie_data_to_json("recommendations.json", data)
-
-
-if __name__ == "__main__":
-    main()
