bc07f971d4f30cb40ad999f12d54d22514807525
vnoelifant <veronicanoel12490@gmail.com>
2023-11-12 20:49:37 -0600

    refactored tmdb_api.py to class

--- movie_search/tmdb_api.py
+++ movie_search/tmdb_api.py
@@ -8,26 +8,21 @@ import requests
 import json
 from django.conf import settings
 
-BASE_URL = "https://api.themoviedb.org/3"
-LANG_ENG = "en-US"
+class TMDBApi:
+    def __init__(self, base_url="https://api.themoviedb.org/3"):
+        self.api_key = settings.TMDB_API_KEY  # Accessing API key from Django settings
+        self.base_url = base_url
+        self.language = "en-US"
 
+    def get_data_from_endpoint(self, endpoint, **kwargs):
+        url = f"{self.base_url}{endpoint}"
+        params = {"api_key": self.api_key, "language": self.language}
+        params.update(kwargs)
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
 
-def get_data_from_endpoint(endpoint, **kwargs):
-    """This function returns a JSON object of tmdb media data."""
-    url = f"{BASE_URL}{endpoint}"
-    params = {"api_key": settings.TMDB_API_KEY, "language": LANG_ENG}
-    params.update(kwargs)
-
-    response = requests.get(url, params=params)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-    print(json.dumps(response.json(), indent=4, sort_keys=True))
-    return response.json()
-
-
-def get_data_by_query(
-    endpoint: str, text_query: str, result_key: str
-) -> dict[str, int]:
-    """Returns a dictionary of details based on a text query."""
-    response_json = get_data_from_endpoint(endpoint, query=text_query)
-    data = response_json["results"]
-    return {row[result_key]: row["id"] for row in data}
+    def get_data_by_query(self, endpoint, text_query, result_key):
+        response_json = self.get_data_from_endpoint(endpoint, query=text_query)
+        data = response_json["results"]
+        return {row[result_key]: row["id"] for row in data}
