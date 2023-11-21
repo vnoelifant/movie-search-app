b067b57575d0ce5d4110aaa972427b26bd0abf78
vnoelifant <veronicanoel12490@gmail.com>
2023-11-21 12:34:57 -0600

    variable and fixture rename

--- movie_search/admin.py
+++ movie_search/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Genre, Movie, Provider, Video, Recommendation, WatchList
+from .models import CustomUser, MovieGenre, Movie, MovieProvider, MovieVideo, MovieRecommendation, MovieWatchList
 
 
 # Register your models here.
-admin.site.register(Genre)
-admin.site.register(Provider)
+admin.site.register(MovieGenre)
+admin.site.register(MovieProvider)
 admin.site.register(Movie)
-admin.site.register(Video)
-admin.site.register(Recommendation)
+admin.site.register(MovieVideo)
+admin.site.register(MovieRecommendation)
 # Register the CustomUser model with the same options as the default User
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(WatchList)
+admin.site.register(MovieWatchList)
