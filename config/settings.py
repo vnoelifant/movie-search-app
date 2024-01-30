ab17adb5b0d6a210240f1d094534b7d0b90e9ca0
vnoelifant <veronicanoel12490@gmail.com>
2024-01-29 23:39:53 -0600

    Update ALLOWED_HOSTS with Heroku app name

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['your-app-name.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['wonder-watch-live.herokuapp.com', 'localhost', '127.0.0.1']
 
 TMDB_API_KEY = config("PROJECT_API_KEY", "")
 TMDB_API_URL = config("TMDB_API_URL", "https://api.themoviedb.org/3")
