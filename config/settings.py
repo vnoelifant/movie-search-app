019373b423d99f330d0a849f7e9373b1f61b4a29
vnoelifant <veronicanoel12490@gmail.com>
2024-01-29 23:52:48 -0600

    added a default value to your local database URI string

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600,
         ssl_require=True
     )
