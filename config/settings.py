8bd38757da9d4c1c76e8f410dd2cad48fcb0983a
vnoelifant <veronicanoel12490@gmail.com>
2024-01-31 01:49:10 -0600

    added release comment and moved ALLOWED_HOSTS to .env

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ import os
 import dj_database_url
 from pathlib import Path
 
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
