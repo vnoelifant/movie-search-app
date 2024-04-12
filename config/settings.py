2fc19c0394aedc7178afa5ce7905b828e14fc7e6
vnoelifant <veronicanoel12490@gmail.com>
2024-04-12 08:13:06 -0500

    views updates

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "movie_search.middleware.TMDBApiMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
